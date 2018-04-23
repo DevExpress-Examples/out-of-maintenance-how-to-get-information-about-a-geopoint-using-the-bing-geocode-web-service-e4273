214f6892bf08740bdd53c8977c3a0fac05c73bdb
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/GetPointLocationInformation/MainPage.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using DevExpress.Xpf.Map;
+
+namespace GetPointLocationInformation {
+    public partial class MainPage : UserControl {
+        double latitude;
+        double longitude;
+
+        public MainPage() {
+            InitializeComponent();
+        }
+        private bool GetLocationArguments() {
+            latitude = String.IsNullOrEmpty(tbLatitude.Text) ? 0 : Double.Parse(tbLatitude.Text);
+            if ((latitude > 90) || (latitude < -90)) {
+                MessageBox.Show("Latitude must be less than or equal to 90 and greater than or equal to - 90. Please, correct the input value.");
+                return false;
+            }
+
+            longitude = String.IsNullOrEmpty(tbLongitude.Text) ? 0 : Double.Parse(tbLongitude.Text);
+            if ((longitude > 180) || (longitude < -180)) {
+                MessageBox.Show("Longitude must be less than or equal to 180 and greater than or equal to - 180. Please, correct the input value.");
+                return false;
+            }
+
+            return true;
+        }
+
+        private void requestLocation_Click(object sender, RoutedEventArgs e) {
+            if (GetLocationArguments()) {
+                LocationRequest();
+            }
+        }
+
+        private void LocationRequest() {
+            LocationInformationRequest(new GeoPoint(latitude, longitude));
+        }
+
+        private void LocationInformationRequest(GeoPoint geoPoint) {
+            try {
+                #region #RequestLocation
+                geocodeProvider.RequestLocationInformation(new GeoPoint(latitude, longitude), 0);
+                #endregion #RequestLocation
+            }
+            catch (Exception ex) {
+                MessageBox.Show("An error occurs: " + ex.ToString());
+            }
+        }
+
+        private void geocodeProvider_LocationInformationReceived(object sender, LocationInformationReceivedEventArgs e) {
+            mapControl1.CenterPoint = new GeoPoint(latitude, longitude);
+            mapControl1.ZoomLevel = 10;
+            GeocodeRequestResult result = e.Result;
+            StringBuilder resultList = new StringBuilder("");
+            resultList.Append(String.Format("Status: {0}\n", result.ResultCode));
+            resultList.Append(String.Format("Fault reason: {0}\n", result.FaultReason));
+
+            if (result.ResultCode == RequestResultCode.Success) {
+                int resCounter = 1;
+
+                foreach (LocationInformation locations in result.Locations) {
+                    resultList.Append(String.Format("Request Result {0}:\n", resCounter));
+                    resultList.Append(String.Format("Display Name: {0}\n", locations.DisplayName));
+                    resultList.Append(String.Format("Entity Type: {0}\n", locations.EntityType));
+                    resultList.Append(String.Format("Address: {0}\n", locations.Address));
+                    resultList.Append(String.Format("Location: {0}\n", locations.Location));
+                    resultList.Append(String.Format("______________________________\n"));
+                    resCounter++;
+                }
+
+            }
+
+            tbResults.Text = resultList.ToString();
+        }
+
+    }
+}
---
+++ VB/GetPointLocationInformation/MainPage.xaml.vb
@@ -0,0 +1,80 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Text
+Imports System.Windows
+Imports System.Windows.Controls
+Imports DevExpress.Xpf.Map
+
+Namespace GetPointLocationInformation
+	Partial Public Class MainPage
+		Inherits UserControl
+		Private latitude As Double
+		Private longitude As Double
+
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+		Private Function GetLocationArguments() As Boolean
+			latitude = If(String.IsNullOrEmpty(tbLatitude.Text), 0, Double.Parse(tbLatitude.Text))
+			If (latitude > 90) OrElse (latitude < -90) Then
+				MessageBox.Show("Latitude must be less than or equal to 90 and greater than or equal to - 90. Please, correct the input value.")
+				Return False
+			End If
+
+			longitude = If(String.IsNullOrEmpty(tbLongitude.Text), 0, Double.Parse(tbLongitude.Text))
+			If (longitude > 180) OrElse (longitude < -180) Then
+				MessageBox.Show("Longitude must be less than or equal to 180 and greater than or equal to - 180. Please, correct the input value.")
+				Return False
+			End If
+
+			Return True
+		End Function
+
+		Private Sub requestLocation_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			If GetLocationArguments() Then
+				LocationRequest()
+			End If
+		End Sub
+
+		Private Sub LocationRequest()
+			LocationInformationRequest(New GeoPoint(latitude, longitude))
+		End Sub
+
+		Private Sub LocationInformationRequest(ByVal geoPoint As GeoPoint)
+			Try
+'				#Region "#RequestLocation"
+				geocodeProvider.RequestLocationInformation(New GeoPoint(latitude, longitude), 0)
+'				#End Region ' #RequestLocation
+			Catch ex As Exception
+				MessageBox.Show("An error occurs: " & ex.ToString())
+			End Try
+		End Sub
+
+		Private Sub geocodeProvider_LocationInformationReceived(ByVal sender As Object, ByVal e As LocationInformationReceivedEventArgs)
+			mapControl1.CenterPoint = New GeoPoint(latitude, longitude)
+			mapControl1.ZoomLevel = 10
+			Dim result As GeocodeRequestResult = e.Result
+			Dim resultList As New StringBuilder("")
+			resultList.Append(String.Format("Status: {0}" & Constants.vbLf, result.ResultCode))
+			resultList.Append(String.Format("Fault reason: {0}" & Constants.vbLf, result.FaultReason))
+
+			If result.ResultCode = RequestResultCode.Success Then
+				Dim resCounter As Integer = 1
+
+				For Each locations As LocationInformation In result.Locations
+					resultList.Append(String.Format("Request Result {0}:" & Constants.vbLf, resCounter))
+					resultList.Append(String.Format("Display Name: {0}" & Constants.vbLf, locations.DisplayName))
+					resultList.Append(String.Format("Entity Type: {0}" & Constants.vbLf, locations.EntityType))
+					resultList.Append(String.Format("Address: {0}" & Constants.vbLf, locations.Address))
+					resultList.Append(String.Format("Location: {0}" & Constants.vbLf, locations.Location))
+					resultList.Append(String.Format("______________________________" & Constants.vbLf))
+					resCounter += 1
+				Next locations
+
+			End If
+
+			tbResults.Text = resultList.ToString()
+		End Sub
+
+	End Class
+End Namespace
